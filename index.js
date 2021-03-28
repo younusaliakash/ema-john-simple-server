adcafe7eae4286c74bbb9f8716c51fdb11129043
Younus Ali Akash <younusaliakash8@gmail.com>
2021-03-29 03:07:09 +0600

    perfectly done server

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
+app.get('/' , (req, res) => {
+    res.send("server is working successfully")
+})
 
 client.connect(err => {
   const collection = client.db("emJohnEStore").collection("products");
